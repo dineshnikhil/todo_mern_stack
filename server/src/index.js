a061051f788143aeb2bb3ad8a6d09eb60f0d7d58
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-05 17:01:11 +0530

    created the simple express server for demo

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const create_and_run_server = () => {
+	const app = express();
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	app.listen(process.env.PORT, () => {
+		console.log('Server is running at port: ', process.env.PORT);
+	});
+};
+
+create_and_run_server();
