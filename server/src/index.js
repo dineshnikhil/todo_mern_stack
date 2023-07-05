48732d34f3c1953b2c7a5149624bf0c2c8045a0f
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-05 17:23:08 +0530

    configered and created the mysql database using sequelize cli

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+
+const { PORT } = require('./configs/serverConfig');
 
 const create_and_run_server = () => {
 	const app = express();
+
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
 
-	app.listen(process.env.PORT, () => {
-		console.log('Server is running at port: ', process.env.PORT);
+	app.listen(PORT, () => {
+		console.log('Server is running at port: ', PORT);
 	});
 };
 
