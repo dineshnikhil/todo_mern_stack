b316dc5208ca33dab1a1ec517ff669862c256977
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-19 09:13:52 +0530

    added some simple styling to change the look of the website.

--- client/src/components/ui/Nav.jsx
+++ client/src/components/ui/Nav.jsx
@@ -4,18 +4,30 @@ import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 
+import './Nav.css';
+
 function Nav() {
 	return (
 		<React.Fragment>
-			<AppBar>
-				<Toolbar>
-					<Link to="/">
-						<h2>DoItNow</h2>
-					</Link>
+			<AppBar
+				sx={{
+					padding: '0.5% 4%',
+				}}
+			>
+				<Toolbar className="navLinkDiv">
+					<div>
+						<Link to="/" className="logo">
+							<h2>🎯 DoItNow</h2>
+						</Link>
+					</div>
 
 					<div>
-						<Link to="/signin">SignIN</Link>
-						<Link to="/signup">SignUp</Link>
+						<Link to="/signin" className="navLink">
+							SignIN
+						</Link>
+						<Link to="/signup" className="navLink">
+							SignUp
+						</Link>
 					</div>
 				</Toolbar>
 			</AppBar>
