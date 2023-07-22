9450122f2409eea78f00a6841b82aa4cf3edfaa3
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-22 11:12:35 +0530

    successfully slove the association issue

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routers/index');
 
+const { User, Todo } = require('./models/index');
 const db = require('./models/index');
 
 const create_and_run_server = () => {
@@ -16,12 +17,31 @@ const create_and_run_server = () => {
 
 	app.use('/api', ApiRoutes);
 
-	app.listen(PORT, () => {
+	app.listen(PORT, async () => {
 		console.log('Server is running at port: ', PORT);
 
-		if (false) {
-			db.sequelize.sync({ alert: true });
-		}
+		// const t1 = await Todo.findByPk(31);
+		// const u1 = await User.findByPk(5);
+
+		// // console.log(t1);
+
+		// t1.addUser(u1);
+
+		const userId = 5;
+
+		User.findByPk(userId).then((user) => {
+			if (!user) {
+				console.log('user not found...!');
+			}
+
+			user.getTodos().then((todos) => {
+				console.log(todos);
+			});
+		});
+
+		// if (true) {
+		// 	db.sequelize.sync({ alert: true });
+		// }
 	});
 };
 
