1d9e141141e24ea53f26b044d5be145438ef640c
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-28 12:05:07 +0530

    added the extra div in the welcome page.

--- client/src/components/Wellcome.jsx
+++ client/src/components/Wellcome.jsx
@@ -8,6 +8,8 @@ import exportDataFeature from '../assets/images/exportDataFeatureImg.svg';
 import wellcomeLogo from '../assets/images/wellcomeLogo.svg';
 
 import { Button } from '@mui/material';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
 
 import FeatureCard from './ui/featureCards/FeatureCard';
 
@@ -16,7 +18,7 @@ function Wellcome() {
 		<>
 			<div className="wellcomeDiv">
 				<div>
-					<h1>Welcome to TaskMaster: Your Ultimate Todo Task Manager</h1>
+					<h1>Welcome to 🎯DoItNow : Your Ultimate Todo Task Manager</h1>
 					<div className="wellcomeActionDiv">
 						<Link to="/signin" className="actionLink">
 							<Button
@@ -43,6 +45,38 @@ function Wellcome() {
 				<div>
 					<img src={wellcomeLogo} alt="simple wellcome logo of the brand." />
 				</div>
+			</div>
+			<div>
+				<Card
+					sx={{
+						backgroundColor: '#EB5E28',
+						color: '#252422',
+						padding: '5% 12%',
+						boxShadow: 5,
+						borderRadius: '1rem',
+					}}
+				>
+					<h1 style={{ fontSize: '2rem' }}>Why you Need this tool..</h1>
+					<Typography
+						sx={{
+							color: '#32312E',
+							fontSize: '1.3rem',
+							fontWeight: 'semiBold',
+						}}
+					>
+						📝 Todo task managers are invaluable tools for enhancing day-to-day
+						productivity. They enable users to organize and prioritize tasks,
+						break down complex projects into manageable steps, and set reminders
+						and deadlines to stay on track. By providing a clear overview of
+						pending and completed tasks, these managers empower users to manage
+						their time efficiently, reduce stress, and accomplish their goals
+						effectively. 💪 Additionally, with seamless collaboration features
+						and accessibility across devices, users can collaborate with teams,
+						access their tasks on the go, and integrate with other productivity
+						tools, making todo task managers essential companions for
+						streamlining daily tasks and optimizing productivity. 🚀
+					</Typography>
+				</Card>
 			</div>
 			<div className="featuresDiv">
 				<h1>DoItNow Features</h1>
